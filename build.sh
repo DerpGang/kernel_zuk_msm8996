2474e37208fe5a8ce20125c88218261bc770dbf4
prometheusXderpgang <prometheusXderpgang@gmail.com>
2020-05-10 06:31:56 +0000

    build.sh: update toolchain folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 make O=out ARCH=arm64 statix_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC=~/clang10/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/gcc10/bin/aarch64-linux-android-  CROSS_COMPILE_ARM32=~/arm32/bin/arm-linux-androideabi-
+make -j$(nproc --all) O=out ARCH=arm64 CC=~/toolchain/clang10/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/toolchain/gcc64/bin/aarch64-linux-android-  CROSS_COMPILE_ARM32=~/toolchain/gcc32/bin/arm-linux-androideabi-
 
 bash zip.sh
