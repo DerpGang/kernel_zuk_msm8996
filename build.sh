5110b482c991348bb2d55697c216d48ffa5c0f4b
prometheusXderpgang <prometheusXderpgang@gmail.com>
2020-05-16 06:42:35 +0000

    defconfig: disablw IOMMU debugging and upadte build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+export KBUILD_BUILD_USER=Kanishk
+
+export KBUILD_BUILD_HOST=DerpGang™
+
 make O=out ARCH=arm64 statix_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC=~/toolchain/clang10/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/toolchain/gcc64/bin/aarch64-linux-android-  CROSS_COMPILE_ARM32=~/toolchain/gcc32/bin/arm-linux-androideabi-
+make -j$(nproc --all) O=out ARCH=arm64 CC=~/toolchain/clang11/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/toolchain/gcc64/bin/aarch64-linux-android-  CROSS_COMPILE_ARM32=~/toolchain/gcc32/bin/arm-linux-androideabi-
 
 bash zip.sh
