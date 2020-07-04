kerneltoast/android_kernel_oneplus_msm8996

power: Add back OnePlus fingerprintd() speed hack

kernel/power/process.c
@@ -221,6 +221,29 @@ int freeze_kernel_threads(void)
 	return error;
 }
 
+#ifdef CONFIG_MACH_MSM8996_15801
+void thaw_fingerprintd(void)
+{
+	struct task_struct *g, *p;
+	struct task_struct *curr = current;
+
+	pm_freezing = false;
+	pm_nosig_freezing = false;
+
+	read_lock(&tasklist_lock);
+	for_each_process_thread(g, p) {
+	/* No other threads should have PF_SUSPEND_TASK set */
+		WARN_ON((p != curr) && (p->flags & PF_SUSPEND_TASK));
+		if (!memcmp(p->comm, "fingerprintd", 13))
+			__thaw_task(p);
+	}
+	read_unlock(&tasklist_lock);
+
+	pm_freezing = true;
+	pm_nosig_freezing = true;
+}
+#endif
+
 void thaw_processes(void)
 {
 	struct task_struct *g, *p;

kernel/power/suspend.c
@@ -41,6 +41,10 @@ static const struct platform_freeze_ops *freeze_ops;
 static DECLARE_WAIT_QUEUE_HEAD(suspend_freeze_wait_head);
 static bool suspend_freeze_wake;
 
+#ifdef CONFIG_MACH_MSM8996_15801
+extern void thaw_fingerprintd(void);
+#endif
+
 void freeze_set_ops(const struct platform_freeze_ops *ops)
 {
 	lock_system_sleep();
@@ -358,6 +362,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Platform_wake:
 	platform_resume_noirq(state);
+#ifdef CONFIG_MACH_MSM8996_15801
+	thaw_fingerprintd();
+#endif
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume:

DerpGang/kernel_zuk_msm8996

Signed-off-by: dd3boh <dade.garberi@gmail.com>
Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

kernel/power/process.c
@@ -221,6 +221,29 @@ int freeze_kernel_threads(void)
 	return error;
 }
 
+#ifdef CONFIG_MACH_ZUK_MSM8996
+void thaw_fingerprintd(void)
+{
+	struct task_struct *g, *p;
+	struct task_struct *curr = current;
+
+	pm_freezing = false;
+	pm_nosig_freezing = false;
+
+	read_lock(&tasklist_lock);
+	for_each_process_thread(g, p) {
+	/* No other threads should have PF_SUSPEND_TASK set */
+		WARN_ON((p != curr) && (p->flags & PF_SUSPEND_TASK));
+		if (!memcmp(p->comm, "fingerprintd", 13))
+			__thaw_task(p);
+	}
+	read_unlock(&tasklist_lock);
+
+	pm_freezing = true;
+	pm_nosig_freezing = true;
+}
+#endif
+
 void thaw_processes(void)
 {
 	struct task_struct *g, *p;

kernel/power/suspend.c
@@ -44,6 +44,10 @@ static DECLARE_WAIT_QUEUE_HEAD(suspend_freeze_wait_head);
 enum freeze_state __read_mostly suspend_freeze_state;
 static DEFINE_SPINLOCK(suspend_freeze_lock);
 
+#ifdef CONFIG_MACH_ZUK_MSM8996
+extern void thaw_fingerprintd(void);
+#endif
+
 void freeze_set_ops(const struct platform_freeze_ops *ops)
 {
 	lock_system_sleep();
@@ -392,6 +396,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Platform_wake:
 	platform_resume_noirq(state);
+#ifdef CONFIG_MACH_ZUK_MSM8996
+	thaw_fingerprintd();
+#endif
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume: